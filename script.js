e240c206dba5bc2ac8e6e7f8195bbd0772fad44a
Michaela Maclean <72842715+mickey-maclean@users.noreply.github.com>
2021-12-17 00:37:53 -0300

    Fix project section

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector("nav");
 const navToogle = document.querySelector(".nav-toogle");
 const toogleIcon = document.querySelector(".toogle-icon");
-
 const anchorLink = document.querySelectorAll(".anchor-link");
 
 navToogle.addEventListener("click", () => {
@@ -11,9 +10,11 @@ navToogle.addEventListener("click", () => {
     if(visibility === "false"){
         menu.setAttribute("data-visible", true);
         toogleIcon.setAttribute("class", "fas fa-times fa-fw toogle-icon");
+        navToogle.style.background = "rgba(0, 0, 0, 0)";
     } else if(visibility === "true"){
         menu.setAttribute("data-visible", false);
         toogleIcon.setAttribute("class", "fas fa-bars fa-fw toogle-icon");
+        navToogle.style.background = "rgb(36, 52, 71, 0.9)";
     }
 } );
 
@@ -21,5 +22,6 @@ anchorLink.forEach( link => {
     link.addEventListener("click", () =>{
         menu.setAttribute("data-visible", false);
         toogleIcon.setAttribute("class", "fas fa-bars fa-fw toogle-icon");
+        navToogle.style.background = "rgb(36, 52, 71, 0.9)";
     });
 });
